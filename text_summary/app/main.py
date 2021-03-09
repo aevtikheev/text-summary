ec52cb5072bf2e2ac9c705512b0b024217c2db3e
aevtikheev <eft000@gmail.com>
2021-03-09 23:43:09 +0300

    Switch to FastAPI router

--- text_summary/app/main.py
+++ text_summary/app/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI, Depends
+import os
+
+from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 
-from app.config import get_settings, Settings
+from app.api import ping
+from app.config import get_settings
 
 app = FastAPI()
 
@@ -14,10 +17,20 @@ register_tortoise(
 )
 
 
-@app.get('/ping')
-async def pong(settings: Settings = Depends(get_settings)):
-    return {
-        'ping': '11',
-        "environment": settings.environment,
-        "testing": settings.testing,
-    }
+def create_application() -> FastAPI:
+    application = FastAPI()
+
+    register_tortoise(
+        application,
+        db_url=os.environ.get("DATABASE_URL"),
+        modules={"models": ["app.models"]},
+        generate_schemas=True,
+        add_exception_handlers=True,
+    )
+
+    application.include_router(ping.router)
+
+    return application
+
+
+app = create_application()
