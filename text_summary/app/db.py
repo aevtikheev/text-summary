f7609882e611519d131617c8f56352dbe434ca49
aevtikheev <eft000@gmail.com>
2021-03-11 23:12:00 +0300

    Move DB interaction to a separate module

---
+++ text_summary/app/db.py
@@ -0,0 +1,36 @@
+import logging
+
+from fastapi import FastAPI
+from tortoise import Tortoise, run_async
+from tortoise.contrib.fastapi import register_tortoise
+
+from app.config import get_settings
+
+
+logger = logging.getLogger('uvicorn')
+
+
+def init_db(app: FastAPI) -> None:
+    register_tortoise(
+        app=app,
+        db_url=get_settings().database_url,
+        modules={'models': ['app.models']},
+        generate_schemas=False,
+        add_exception_handlers=True,
+    )
+
+
+async def generate_schema() -> None:
+    logger.info('Initializing Tortoise...')
+
+    await Tortoise.init(
+        db_url=get_settings().database_url,
+        modules={'models': ['app.models']},
+    )
+    logger.info('Generating database schema via Tortoise...')
+    await Tortoise.generate_schemas()
+    await Tortoise.close_connections()
+
+
+if __name__ == '__main__':
+    run_async(generate_schema())
--- text_summary/app/main.py
+++ text_summary/app/main.py
@@ -1,36 +1,30 @@
-import os
+import logging
 
 from fastapi import FastAPI
-from tortoise.contrib.fastapi import register_tortoise
 
 from app.api import ping
-from app.config import get_settings
+from app.db import init_db
 
-app = FastAPI()
 
-register_tortoise(
-    app=app,
-    db_url=get_settings().database_url,
-    modules={"models": ["app.models"]},
-    generate_schemas=True,
-    add_exception_handlers=True,
-)
+logger = logging.getLogger('uvicorn')
 
 
 def create_application() -> FastAPI:
     application = FastAPI()
-
-    register_tortoise(
-        application,
-        db_url=os.environ.get("DATABASE_URL"),
-        modules={"models": ["app.models"]},
-        generate_schemas=True,
-        add_exception_handlers=True,
-    )
-
     application.include_router(ping.router)
 
     return application
 
 
 app = create_application()
+
+
+@app.on_event('startup')
+async def startup_event() -> None:
+    logger.info('Starting up...')
+    init_db(app)
+
+
+@app.on_event('shutdown')
+async def startup_event() -> None:
+    logger.info('Shutting down...')
