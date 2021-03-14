6872d6e3c83b9ee74b78e69ee3d5ca0e5eba4229
aevtikheev <eft000@gmail.com>
2021-03-14 20:21:25 +0300

    Rework test cases for summaries

--- text_summary/tests/conftest.py
+++ text_summary/tests/conftest.py
@@ -20,14 +20,6 @@ def get_settings_override():
 
 
 @pytest.fixture(scope='module')
-def test_app():
-    app = create_application()
-    app.dependency_overrides[get_settings] = get_settings_override
-    with TestClient(app) as test_client:
-        yield test_client
-
-
-@pytest.fixture(scope='module')
 def test_app_with_db():
     app = create_application()
     app.dependency_overrides[get_settings] = get_settings_override
--- text_summary/tests/test_summaries.py
+++ text_summary/tests/test_summaries.py
@@ -3,7 +3,8 @@ import json
 
 import pytest
 
-SUMMARIES_ENDPOINT = '/summaries'
+SUMMARIES_ENDPOINT = 'summaries'
+
 ID_FIELD = 'id'
 URL_FIELD = 'url'
 SUMMARY_FIELD = 'summary'
@@ -15,32 +16,13 @@ def test_create_summary(test_app_with_db):
     summary_url = 'http://example.com'
     response = test_app_with_db.post(
         f'{SUMMARIES_ENDPOINT}/',
-        data=json.dumps({URL_FIELD: summary_url})
+        data=json.dumps({URL_FIELD: summary_url}),
     )
 
     assert response.status_code == 201, f'Invalid response code: {response.status_code}'
     assert response.json()[URL_FIELD] == summary_url, (
-        f'Invalid summary URL: {response.json()[URL_FIELD]}'
+        f'Invalid summary URL: {response.json()[URL_FIELD]}',
     )
-
-
-@pytest.mark.negative
-def test_create_summary_empty_payload(test_app_with_db):
-    response = test_app_with_db.post(f'{SUMMARIES_ENDPOINT}/', data=json.dumps({}))
-
-    assert response.status_code == 422, f'Invalid response code: {response.status_code}'
-    assert response.json().get(ERROR_DETAIL_FIELD), 'Details about the error are not provided'
-
-
-@pytest.mark.negative
-def test_create_summary_incorrect_url(test_app_with_db):
-    response = test_app_with_db.post(
-        f'{SUMMARIES_ENDPOINT}/',
-        data=json.dumps({URL_FIELD: 'invalid://url'})
-    )
-
-    assert response.status_code == 422, f'Invalid response code: {response.status_code}'
-    assert response.json().get(ERROR_DETAIL_FIELD), 'Details about the error are not provided'
 
 
 def test_read_summary(test_app_with_db, existing_summary):
@@ -70,37 +52,13 @@ def test_read_all_summaries(test_app_with_db, existing_summary):
     )
 
 
-@pytest.mark.negative
-def test_read_nonexistent_summary(test_app_with_db):
-    response = test_app_with_db.get(f'{SUMMARIES_ENDPOINT}/9999999/')
-
-    assert response.status_code == 404, f'Invalid response code: {response.status_code}'
-    assert response.json().get(ERROR_DETAIL_FIELD), 'Details about the error are not provided'
-
-
-@pytest.mark.negative
-def test_read_summary_incorrect_id(test_app_with_db):
-    response = test_app_with_db.get(f'{SUMMARIES_ENDPOINT}/abc/')
-
-    assert response.status_code == 422, f'Invalid response code: {response.status_code}'
-    assert response.json().get(ERROR_DETAIL_FIELD), 'Details about the error are not provided'
-
-
-@pytest.mark.negative
-def test_read_summary_zero_id(test_app_with_db):
-    response = test_app_with_db.get(f'{SUMMARIES_ENDPOINT}/0/')
-
-    assert response.status_code == 422, f'Invalid response code: {response.status_code}'
-    assert response.json().get(ERROR_DETAIL_FIELD), 'Details about the error are not provided'
-
-
 def test_update_summary(test_app_with_db, existing_summary):
     summary_id, summary_url = existing_summary
 
     new_summary = 'updated_summary'
     response = test_app_with_db.put(
         f'{SUMMARIES_ENDPOINT}/{summary_id}/',
-        data=json.dumps({URL_FIELD: summary_url, SUMMARY_FIELD: new_summary})
+        data=json.dumps({URL_FIELD: summary_url, SUMMARY_FIELD: new_summary}),
     )
 
     assert response.status_code == 200, f'Invalid response code: {response.status_code}'
@@ -109,102 +67,9 @@ def test_update_summary(test_app_with_db, existing_summary):
     assert response_json[ID_FIELD] == summary_id, f'Invalid id field: {response_json[ID_FIELD]}'
     assert response_json[URL_FIELD] == summary_url, f'Invalid url field: {response_json[URL_FIELD]}'
     assert response_json[SUMMARY_FIELD] == new_summary, (
-        f'Invalid summary field: {response_json[SUMMARY_FIELD]}'
+        f'Invalid summary field: {response_json[SUMMARY_FIELD]}',
     )
     assert response_json.get(CREATED_AT_FIELD), 'Missing or empty created_at field'
-
-
-@pytest.mark.negative
-def test_update_nonexistent_summary(test_app_with_db, existing_summary):
-    summary_id, summary_url = existing_summary
-
-    response = test_app_with_db.put(
-        f'{SUMMARIES_ENDPOINT}/9999999/',
-        data=json.dumps({URL_FIELD: summary_url, SUMMARY_FIELD: 'updated_summary'})
-    )
-
-    assert response.status_code == 404, f'Invalid response code: {response.status_code}'
-    assert response.json().get(ERROR_DETAIL_FIELD), 'Details about the error are not provided'
-
-
-@pytest.mark.negative
-def test_update_summary_incorrect_id(test_app_with_db, existing_summary):
-    summary_id, summary_url = existing_summary
-
-    response = test_app_with_db.put(
-        f'{SUMMARIES_ENDPOINT}/abc/',
-        data=json.dumps({URL_FIELD: summary_url, SUMMARY_FIELD: 'updated_summary'})
-    )
-
-    assert response.status_code == 422, f'Invalid response code: {response.status_code}'
-    assert response.json().get(ERROR_DETAIL_FIELD), 'Details about the error are not provided'
-
-
-@pytest.mark.negative
-def test_update_summary_zero_id(test_app_with_db, existing_summary):
-    summary_id, summary_url = existing_summary
-
-    response = test_app_with_db.put(
-        f'{SUMMARIES_ENDPOINT}/0/',
-        data=json.dumps({URL_FIELD: summary_url, SUMMARY_FIELD: 'updated_summary'})
-    )
-
-    assert response.status_code == 422, f'Invalid response code: {response.status_code}'
-    assert response.json().get(ERROR_DETAIL_FIELD), 'Details about the error are not provided'
-
-
-@pytest.mark.negative
-def test_update_summary_missing_url(test_app_with_db, existing_summary):
-    summary_id, summary_url = existing_summary
-
-    new_summary = 'updated_summary'
-    response = test_app_with_db.put(
-        f'{SUMMARIES_ENDPOINT}/{summary_id}/',
-        data=json.dumps({SUMMARY_FIELD: new_summary})
-    )
-
-    assert response.status_code == 422, f'Invalid response code: {response.status_code}'
-    assert response.json().get(ERROR_DETAIL_FIELD), 'Details about the error are not provided'
-
-
-@pytest.mark.negative
-def test_update_summary_incorrect_url(test_app_with_db, existing_summary):
-    summary_id, summary_url = existing_summary
-
-    new_summary = 'updated_summary'
-    response = test_app_with_db.put(
-        f'{SUMMARIES_ENDPOINT}/{summary_id}/',
-        data=json.dumps({SUMMARY_FIELD: new_summary, URL_FIELD: 'invalid://url'})
-    )
-
-    assert response.status_code == 422, f'Invalid response code: {response.status_code}'
-    assert response.json().get(ERROR_DETAIL_FIELD), 'Details about the error are not provided'
-
-
-@pytest.mark.negative
-def test_update_summary_missing_summary(test_app_with_db, existing_summary):
-    summary_id, summary_url = existing_summary
-
-    response = test_app_with_db.put(
-        f'{SUMMARIES_ENDPOINT}/{summary_id}/',
-        data=json.dumps({URL_FIELD: summary_url})
-    )
-
-    assert response.status_code == 422, f'Invalid response code: {response.status_code}'
-    assert response.json().get(ERROR_DETAIL_FIELD), 'Details about the error are not provided'
-
-
-@pytest.mark.negative
-def test_update_summary_empty_payload(test_app_with_db, existing_summary):
-    summary_id, summary_url = existing_summary
-
-    response = test_app_with_db.put(
-        f'{SUMMARIES_ENDPOINT}/{summary_id}/',
-        data=json.dumps({})
-    )
-
-    assert response.status_code == 422, f'Invalid response code: {response.status_code}'
-    assert response.json().get(ERROR_DETAIL_FIELD), 'Details about the error are not provided'
 
 
 def test_delete_summary(test_app_with_db, existing_summary):
@@ -216,24 +81,78 @@ def test_delete_summary(test_app_with_db, existing_summary):
 
 
 @pytest.mark.negative
-def test_delete_summary_incorrect_id(test_app_with_db):
-    response = test_app_with_db.delete(f'{SUMMARIES_ENDPOINT}/abc/')
+@pytest.mark.parametrize(
+    'payload',
+    [{}, {URL_FIELD: 'invalid://url'}],
+    ids=['empty payload', 'incorrect url'],
+)
+def test_create_summary_incorrect_payload(test_app_with_db, payload):
+    response = test_app_with_db.post(f'{SUMMARIES_ENDPOINT}/', data=json.dumps({}))
 
     assert response.status_code == 422, f'Invalid response code: {response.status_code}'
     assert response.json().get(ERROR_DETAIL_FIELD), 'Details about the error are not provided'
 
 
 @pytest.mark.negative
-def test_delete_summary_zero_id(test_app_with_db):
-    response = test_app_with_db.delete(f'{SUMMARIES_ENDPOINT}/0/')
+@pytest.mark.parametrize(
+    'summary_id,response_code',
+    [('abc', 422), ('0', 422), ('99999999', 404)],
+    ids=['non-digit ID', 'zero ID', 'Nonexistent ID'],
+)
+def test_read_summary_incorrect_id(test_app_with_db, summary_id, response_code):
+    response = test_app_with_db.get(f'{SUMMARIES_ENDPOINT}/{summary_id}/')
+
+    assert response.status_code == response_code, f'Invalid response code: {response.status_code}'
+    assert response.json().get(ERROR_DETAIL_FIELD), 'Details about the error are not provided'
+
+
+@pytest.mark.negative
+@pytest.mark.parametrize(
+    'summary_id,response_code',
+    [('abc', 422), ('0', 422), ('99999999', 404)],
+    ids=['non-digit ID', 'zero ID', 'Nonexistent ID'],
+)
+def test_update_summary_incorrect_id(test_app_with_db, existing_summary, summary_id, response_code):
+    response = test_app_with_db.put(
+        f'{SUMMARIES_ENDPOINT}/{summary_id}/',
+        data=json.dumps({URL_FIELD: 'http://example.com', SUMMARY_FIELD: 'updated_summary'}),
+    )
+
+    assert response.status_code == response_code, f'Invalid response code: {response.status_code}'
+    assert response.json().get(ERROR_DETAIL_FIELD), 'Details about the error are not provided'
+
+
+@pytest.mark.negative
+@pytest.mark.parametrize(
+    'payload',
+    [
+        {SUMMARY_FIELD: 'new_summary'},
+        {SUMMARY_FIELD: 'new_summary', URL_FIELD: 'invalid://url'},
+        {URL_FIELD: 'http://example.com'},
+        {},
+    ],
+    ids=['Missing URL', 'Incorrect URL', 'Missing summary', 'Empty payload'],
+)
+def test_update_summary_incorrect_payload(test_app_with_db, existing_summary, payload):
+    summary_id, summary_url = existing_summary
+
+    response = test_app_with_db.put(
+        f'{SUMMARIES_ENDPOINT}/{summary_id}/',
+        data=json.dumps(payload),
+    )
 
     assert response.status_code == 422, f'Invalid response code: {response.status_code}'
     assert response.json().get(ERROR_DETAIL_FIELD), 'Details about the error are not provided'
 
 
 @pytest.mark.negative
-def test_delete_nonexistent_summary(test_app_with_db):
-    response = test_app_with_db.delete(f'{SUMMARIES_ENDPOINT}/9999999/')
+@pytest.mark.parametrize(
+    'summary_id,response_code',
+    [('abc', 422), ('0', 422), ('99999999', 404)],
+    ids=['non-digit ID', 'zero ID', 'Nonexistent ID'],
+)
+def test_delete_summary_incorrect_id(test_app_with_db, summary_id, response_code):
+    response = test_app_with_db.delete(f'{SUMMARIES_ENDPOINT}/{summary_id}/')
 
-    assert response.status_code == 404, f'Invalid response code: {response.status_code}'
+    assert response.status_code == response_code, f'Invalid response code: {response.status_code}'
     assert response.json().get(ERROR_DETAIL_FIELD), 'Details about the error are not provided'
