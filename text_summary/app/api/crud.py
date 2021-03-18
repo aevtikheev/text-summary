cf12e42039d550033edc5a0af970136480a3ab9a
aevtikheev <eft000@gmail.com>
2021-03-18 23:21:29 +0300

    Prettify variable names in crud.py

--- text_summary/app/api/crud.py
+++ text_summary/app/api/crud.py
@@ -11,8 +11,8 @@ async def create(payload: SummaryPayloadSchema) -> int:
     return summary.id
 
 
-async def read(id_: int) -> Optional[dict]:
-    summary = await TextSummary.filter(id=id_).first().values()
+async def read(summary_id: int) -> Optional[dict]:
+    summary = await TextSummary.filter(id=summary_id).first().values()
 
     return summary[0] if summary else None
 
@@ -21,15 +21,15 @@ async def read_all() -> List:
     return await TextSummary.all().values()
 
 
-async def update(id_: int, payload: SummaryUpdatePayloadSchema) -> Optional[dict]:
-    summary = await TextSummary.filter(id=id_).first().update(
+async def update(summary_id: int, payload: SummaryUpdatePayloadSchema) -> Optional[dict]:
+    summary = await TextSummary.filter(id=summary_id).first().update(
         url=payload.url, summary=payload.summary,
     )
     if summary:
-        updated_summary = await TextSummary.filter(id=id_).first().values()
+        updated_summary = await TextSummary.filter(id=summary_id).first().values()
         return updated_summary[0]
     return None
 
 
-async def delete(id_: int) -> Optional[dict]:
-    return await TextSummary.filter(id=id_).first().delete()
+async def delete(summary_id: int) -> Optional[dict]:
+    return await TextSummary.filter(id=summary_id).first().delete()
